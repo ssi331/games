42b1ff22b371717d8bf0724a993b9905a3e7998e
highchi81 <hiraihaichiro@gmail.com>
2018-07-13 19:08:24 +0900

    remove

--- test_fizz_buzz.py
+++
@@ -1,8 +0,0 @@
-print(11235)
-
-i = int
-for i in range(0, 100, 1):
-    if i %3 ==0:
-        print("fizz")
-    elif i %5 ==0:
-        print("Buzz")
--- window.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-
-root = Tk()
-root.title("マインスイーパ")
-root.resizable(0,0)
-
-root.mainloop()
